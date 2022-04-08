a9dcc9d5b8e495085fd4a1ede002a56bad8d0c9d
JoelCAI <joelargentina12345@gmail.com>
2022-04-08 14:54:45 -0300

    Agregar archivos de proyecto.

---
+++ ValoresNumericos19/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValoresNumericos19
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Sistema s = new Sistema();
+			s.Iniciar();
+		}
+
+	}
+}
---
+++ ValoresNumericos19/Sistema.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValoresNumericos19
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            decimal numeroUno;
+
+            Validador.Bienvenida();
+
+            numeroUno = Validador.PedirNumeroDecimal("\n Ingrese un valor numérico.");
+
+            Console.Clear();
+
+
+            decimal numeroFinal = decimal.Truncate(numeroUno);
+
+            Console.Clear();
+            Console.WriteLine(" \n Usted Ingreso el numero: *" + numeroUno + "*. \n Del número ingresado su parte entera es: " 
+                               + numeroFinal);
+
+            Console.WriteLine(" \n Si el resultado no fue el esperado es porque ignoró la recomendación e ingreso" +
+                              " un punto en vez de una coma");
+
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
